340a56878f3a48f97fce8c43c3eeac7680a99bdd
Loïs Di Qual <lois.diqual@gmail.com>
2012-07-09 17:55:28 +0200

    Added the ability to build in a different directory

--- lib/wox/builder.rb
+++ lib/wox/builder.rb
@@ -6,13 +6,14 @@ module Wox
     def build
       configuration = environment[:configuration]
       sdk = environment[:sdk]
+      build_dir = File.join environment[:build_dir], "#{configuration}-#{sdk}"
       puts "Building #{environment[:full_name]} configuration:#{configuration}"
 
       arch = sdk == "iphonesimulator" ? " -arch i386" : ""
       
       log_file = File.join environment[:build_dir], "build-#{configuration}.log"
 
-      run_command "xcodebuild -target '#{environment[:target]}' -configuration #{configuration} -sdk #{sdk}#{arch}", :results => log_file
+      run_command "xcodebuild -target '#{environment[:target]}' -configuration #{configuration} -sdk #{sdk}#{arch} CONFIGURATION_BUILD_DIR='#{build_dir}'", :results => log_file
     end
   end
 end
